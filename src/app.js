d1a9404962492eb7646efed9c7f52c11b738440a
Zac Collier <zacacollier@gmail.com>
2017-06-10 23:23:34 -0500

    add more mock data

--- src/app.js
+++ src/app.js
@@ -1,16 +1,48 @@
 (function() {
   // eslint-disable-next-line
   const app = angular.module('FriendsList', []);
-  let myFriends = [{
-    id: 1,
-    name: 'Jerry Seinfeld',
-    occupation: 'Comedian',
-    info: {
-      description: 'Kind of an asshole',
-      trivia: 'Once dated a supermodel',
-      nemesis: 'Newman'
-    }
-  }];
+  let myFriends = [
+    {
+      id: 1,
+      name: 'Jerry Seinfeld',
+      occupation: 'Comedian',
+      info: {
+        description: 'Kind of an asshole',
+        trivia: 'Once dated a supermodel',
+        nemesis: 'Newman'
+      }
+    },
+    {
+      id: 2,
+      name: 'George Costanza',
+      occupation: 'Architect',
+      info: {
+        description: 'Pathological Liar',
+        trivia: 'Had a short-lived career as a hand model.',
+        nemesis: 'His mother'
+      }
+    },
+    {
+      id: 3,
+      name: 'Elaine Benes',
+      occupation: 'Associate Editor',
+      info: {
+        description: 'Tragic Narcissist',
+        trivia: 'Got drunk at a wedding in India once.',
+        nemesis: 'Her neighbor\'s dog.'
+      }
+    },
+    {
+      id: 4,
+      name: 'Cosmo Kramer',
+      occupation: 'unknown',
+      info: {
+        description: 'Quintessential 90s Hipster',
+        trivia: 'Prefers Jockeys',
+        nemesis: 'The librarian.'
+      }
+    },
+  ];
   app.controller('FriendsController', function () {
     this.friends = myFriends;
   });
